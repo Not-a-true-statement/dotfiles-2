f7aa38c044a2106cfb827e3df08f73a35ce56761
Kalos Lazo <lzm.kalos@gmail.com>
2023-01-01 19:36:09 -0500

    :mag: update autostart

--- qtile/settings/autostart.py
+++ qtile/settings/autostart.py
@@ -1,6 +1,6 @@
 # ///--- autostart config -> all instructions will be executed at the command line every reload.
 # ///--- change four your tweaks settings.
 initialCmdArr = [
-    "feh --bg-fill ~/Pictures/wallpapers/wp2.jpg",
-    "xrandr --output DisplayPort-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-0 --off --output DVI-1 --off"
+    "feh --bg-fill ~/Pictures/wallpapers/wp4.jpg",
+    "xrandr --output DisplayPort-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-0 --off --output DVI-1 --off",
     ];
